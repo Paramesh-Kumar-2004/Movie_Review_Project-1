5d6accbd3411c4c22a941b2cf6186c9ce2e58677
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-11-17 23:22:39 +0530

    Writing HandleRatingUpdate Function

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -4,13 +4,22 @@ const Rating = () => {
 
     const [rating, setRating] = useState(Math.round(Math.random(5) * 5))
 
+    const HandleRatingUpdate = (e) => {
+        if (e.target.value <= 5) {
+            return setRating(e.target.value)
+        }
+        else {
+            alert("Enter 1 to 5")
+        }
+    }
+
     return (
         <div>
             <input
                 type="text"
                 value={rating}
                 placeholder='Enter The Number 1 to 5'
-                onChange={(e) => { if (e.target.value <= 5) return setRating(e.target.value); else return alert("Only 1 to 5") }}
+                onChange={(e) => { HandleRatingUpdate(e) }}
                 className='border-2 border-sky-500 p-0.5'
             />
             Rating : {rating}
